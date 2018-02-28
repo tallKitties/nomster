1170a601c98e03615bf04155b4f6392c511427a7
tallKitties <bkautt73@gmail.com>
2018-02-28 02:32:09 +0000

    added geocoding

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,5 +1,8 @@
 class Place < ApplicationRecord
   belongs_to :user
+  geocoded_by :address
+  after_validation :geocode
+
   validates :address, :description, :name, presence: { message: "This field is required." }
   validates :name, length: { minimum: 4 }
 end
